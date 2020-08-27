2dea97493559a5dbe177bfeda0cbf9986bdf54d7
Tianning Li <ltianningli@gmail.com>
2020-08-26 21:51:38 -0700

    Updates

---
+++ precache-manifest.e68eeb84a8679aeccbb027d153ee301a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39a76eff895fb6592e544950b0d340c2",
+    "url": "/youtube_clone/index.html"
+  },
+  {
+    "revision": "42d79eacf2c7c1f83b4f",
+    "url": "/youtube_clone/static/css/main.2079c1c0.chunk.css"
+  },
+  {
+    "revision": "e52bb17021a9e7fd20d4",
+    "url": "/youtube_clone/static/js/2.331f03d1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/youtube_clone/static/js/2.331f03d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42d79eacf2c7c1f83b4f",
+    "url": "/youtube_clone/static/js/main.48b483f6.chunk.js"
+  },
+  {
+    "revision": "0c73943a78a1a4e1f18c",
+    "url": "/youtube_clone/static/js/runtime-main.6babd95e.js"
+  }
+]);
